2837c6f325e33c363aa53c6c9a3bc2d72b852128
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2024-03-15 22:50:10 -0300

    modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,14 +1,50 @@
-export function NoteCard() {
-  return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2  hover:ring-slate-600 focus-ring-2 focus-ring-lime-400 outline-none">
-      <span className="text-sm font-medium text-slate-300">Adicionar nota</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint aperiam
-        iusto laudantium. Pariatur debitis autem rem magni nemo error ab ipsum
-        rerum non magnam labore quod enim, culpa alias blanditiis.
-      </p>
+import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 from-black/0 pointer-events-none" />
-    </button>
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md text-left flex flex-col  bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2  hover:ring-slate-600 focus-ring-2 focus-ring-lime-400 outline-none">
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toLocaleDateString("pt-BR")}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 from-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black/50 inset-0 fixed" />
+        <Dialog.Content className="fixed overflow-hidden top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] rounded-md bg-slate-700 flex flex-col outline-none">
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-300">
+              {formatDistanceToNow(note.date, {
+                locale: ptBR,
+                addSuffix: true,
+              })}
+            </span>
+
+            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+          </div>
+
+          <button
+            type="button"
+            className="w-full py-4 bg-slate-800 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja
+            <span className="text-red-400 group-hover:underline">
+              excluir essa nota?
+            </span>
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
