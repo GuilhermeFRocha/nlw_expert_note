26089c4355c34835a505eb7dc76330a22bc60cdd
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2024-03-17 11:15:59 -0300

    edit modal

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,10 +1,70 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+import { ChangeEvent, useState } from "react";
+
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
+
+  function handleContentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    if (event.target.value === "") {
+      setShouldShowOnboarding(true);
+    }
+  }
+
   return (
-    <div className="rounded-md bg-slate-700 p-5 space-y-3">
-      <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
-      <p className="text-sm leading-6 text-slate-400">
-        askndfaisbhiasbgdagsdygasydaso8dgasyg
-      </p>
-    </div>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 outline-none hover:ring-2  hover:ring-slate-600 focus-ring-2 focus-ring-lime-400 outline-none">
+        <span className="text-sm font-medium text-slate-200">
+          Adicionar nota
+        </span>
+        <p className="text-sm leading-6 text-slate-400">
+          askndfaisbhiasbgdagsdygasydaso8dgasyg
+        </p>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black/50 inset-0 fixed" />
+        <Dialog.Content className="fixed overflow-hidden top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] rounded-md bg-slate-700 flex flex-col outline-none">
+          <Dialog.Close className="absolute top-0 right-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-300">
+              Adicionar nota
+            </span>
+            {shouldShowOnboarding ? (
+              <p className="text-sm leading-6 text-slate-400">
+                Comece{" "}
+                <button className="font-medium text-lime-400 hover:underline">
+                  gravando uma nota{" "}
+                </button>{" "}
+                em audio ou se preferir{" "}
+                <button
+                  onClick={() => setShouldShowOnboarding(false)}
+                  className="font-medium text-lime-400 hover:underline"
+                >
+                  utilize apenas texto
+                </button>
+                .
+              </p>
+            ) : (
+              <textarea
+                autoFocus
+                className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                onChange={handleContentChange}
+              />
+            )}
+          </div>
+
+          <button
+            type="button"
+            className="w-full py-4 bg-lime-400 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+          >
+            Salvar nota
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { X } from "lucide-react";
 
 interface NoteCardProps {
   note: {
@@ -23,6 +24,10 @@ export function NoteCard({ note }: NoteCardProps) {
       <Dialog.Portal>
         <Dialog.Overlay className="bg-black/50 inset-0 fixed" />
         <Dialog.Content className="fixed overflow-hidden top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] rounded-md bg-slate-700 flex flex-col outline-none">
+          <Dialog.Close className="absolute top-0 right-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+
           <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-300">
               {formatDistanceToNow(note.date, {
@@ -38,7 +43,7 @@ export function NoteCard({ note }: NoteCardProps) {
             type="button"
             className="w-full py-4 bg-slate-800 text-center text-sm text-slate-300 outline-none font-medium group"
           >
-            Deseja
+            Deseja {""}
             <span className="text-red-400 group-hover:underline">
               excluir essa nota?
             </span>
